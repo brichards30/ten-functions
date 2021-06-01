3290d3a8e182f0446431c77045a5997b3581baf9
Brittany Richards <brittany.m.richards3@gmail.com>
2021-06-01 15:15:55 -0500

    Ten Functions in progress

--- solutions.js
+++ solutions.js
@@ -20,3 +20,58 @@
  * and
  * concat
  */
+
+function isTrue(value) {
+    return value === true;
+}
+
+function isFalse(value) {
+    return value === false;
+}
+
+function not(value) {
+    return !value;
+}
+
+/*function addOne(value) {
+    return value++;
+}*/
+/*  addOne(0)                    // 1
+ addOne(2)                    // 3
+ addOne(-5)                   // -4
+ addOne(5.789)                // 6.789
+ addOne(Infinity)             // Infinity
+ addOne("2")                  // 3
+ addOne("0")                  // 1
+ addOne("banana")             // NaN
+ addOne(true)                 // NaN
+ addOne(NaN)                  // NaN
+
+ HAVING ISSUES*/
+function isEven(value) {
+    return value % 2 === 0;
+}
+
+function isIdentical(value) {
+    return value === value;
+
+}
+
+function or(value) {
+    return value || value;
+}
+
+function isEqual(value) {
+    return value === "value";
+} // having issues
+
+function and(value) {
+    return value && value;
+}
+
+function concat(value) {
+    return value + value;
+}
+/*
+
+ */
